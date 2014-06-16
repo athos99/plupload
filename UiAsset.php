b56e0e274809d3a5fffbc8c19a87034ed05f8561
athos99 <athos99@bluewin.ch>
2014-06-16 23:53:14 +0200

    dev

--- UiAsset.php
+++ UiAsset.php
@@ -2,9 +2,9 @@
 namespace athos99\plupload;
 use yii\web\AssetBundle;
 
-class Asset extends AssetBundle
+class UiAsset extends AssetBundle
 {
-    public $sourcePath = '@app/extensions/plupload/assets';
+    public $sourcePath = '@vendor/athos99/plupload/assets';
     public $depends = array(
         'yii/jui/theme/base/progressbar',
         'yii/jui/progressbar',
